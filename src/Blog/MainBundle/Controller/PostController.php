cb91bc823914fc2934193399c051dff89bb35195
Jaron <jaronjking@gmail.com>
2015-12-09 17:10:05 -0800

    Added pagination to blog.

--- src/Blog/MainBundle/Controller/PostController.php
+++ src/Blog/MainBundle/Controller/PostController.php
@@ -11,13 +11,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends Controller
 {
-    public function postAction(Request $request, $id)
+    public function postAction(Request $request, $id, $page)
     {
         $em = $this->getDoctrine()->getManager();
         $post = $em->getRepository('BlogAdminBundle:Post')->find($id);
         if (!$post) {
             return $this->render('BlogMainBundle:Default:notFound.html.twig');
         }
+        $blogbody = explode('<br>', $post->getBody());
+        $pager = $page * 7 - 7;
+        $pageMax = ceil(count($blogbody)/10);
         $messages = $post->getMessages();
         $entity = new Message;
         $form = $this->createForm(new MessageType(), $entity, array(
@@ -40,6 +43,10 @@ class PostController extends Controller
             'post' => $post,
             'messages' => $messages,
             'form' => $form->createView(),
+            'page' => $page,
+            'blogbody' => $blogbody,
+            'pager' => $pager,
+            'pageMax' => $pageMax
         ));
     }
 
